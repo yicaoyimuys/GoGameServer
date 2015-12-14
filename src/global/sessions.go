60a28763622bca8aeae54ed583b172e2e701c26b
yangsong <4707384@qq.com>
2015-12-14 16:58:45 +0800

    sessions修改，添加FetchSession函数

--- src/global/sessions.go
+++ src/global/sessions.go
@@ -8,18 +8,15 @@ import (
 var (
 	sessions 		map[uint64]*link.Session = make(map[uint64]*link.Session)
 	sessionMutex 	sync.Mutex
-	sessionNum 		uint32 = 0
 )
 
 func AddSession(session *link.Session) {
 	sessionMutex.Lock()
 	defer sessionMutex.Unlock()
 
-	sessionNum += 1
 	sessions[session.Id()] = session
 	session.AddCloseCallback(session, func() {
 		RemoveSession(session.Id())
-		sessionNum -= 1
 	})
 }
 
@@ -27,17 +24,29 @@ func RemoveSession(key uint64) {
 	sessionMutex.Lock()
 	defer sessionMutex.Unlock()
 
-	if _, exists := sessions[key]; exists {
-		delete(sessions, key)
-	}
+	delete(sessions, key)
 }
 
 func GetSession(key uint64) *link.Session {
 	sessionMutex.Lock()
 	defer sessionMutex.Unlock()
 
-	if session, exists := sessions[key]; exists {
-		return session
+	session, _ := sessions[key]
+	return session
+}
+
+func SessionLen() int {
+	sessionMutex.Lock()
+	defer sessionMutex.Unlock()
+
+	return len(sessions)
+}
+
+func FetchSession(callback func(*link.Session)) {
+	sessionMutex.Lock()
+	defer sessionMutex.Unlock()
+
+	for _, session := range sessions {
+		callback(session)
 	}
-	return nil
 }
